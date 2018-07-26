7bc0e2d8da35c67bef3123da9af36f7fc9c6b2ca
DimNS <atomcms@ya.ru>
2018-07-26 11:34:25 +0500

    Удалён шлюз GatewayP1SMS и реализована возможность создавать свои шлюзы отправки sms

--- app/Configs/SettingsSMS.php
+++ app/Configs/SettingsSMS.php
@@ -2,7 +2,7 @@
 /**
  * Параметры SMS
  *
- * @version 01.01.2018
+ * @version 26.07.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -16,23 +16,7 @@ namespace Lemurro\Api\App\Configs;
 class SettingsSMS
 {
     /**
-     * Шлюз по умолчанию
+     * API-ключ от аккаунта в sms.ru
      */
-    const DEFAULT_GATEWAY = 'smsru';
-
-    /**
-     * Шлюзы для отправки sms
-     * p1sms - p1sms.ru
-     * smsru - sms.ru
-     */
-    const GATEWAYS = [
-        'p1sms' => [
-            'user'     => 'user',
-            'password' => 'password',
-            'sender'   => 'sender',
-        ],
-        'smsru' => [
-            'api_id' => 'api_id',
-        ],
-    ];
+    const SMSRU_API_ID = 'api_id';
 }
