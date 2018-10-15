91fb5a99432e0391ec2c8e12266a8cfe2c82aaea
DimNS <atomcms@ya.ru>
2018-10-15 10:33:58 +0500

    При отправке SMS можно указать буквенного отправителя

--- app/Configs/SettingsSMS.php
+++ app/Configs/SettingsSMS.php
@@ -2,7 +2,7 @@
 /**
  * Параметры SMS
  *
- * @version 26.07.2018
+ * @version 15.10.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -19,4 +19,9 @@ class SettingsSMS
      * API-ключ от аккаунта в sms.ru
      */
     const SMSRU_API_ID = 'api_id';
+
+    /**
+     * Отправитель (можно оставить пустым, если не нужен)
+     */
+    const SMSRU_SENDER = 'SenderName';
 }
