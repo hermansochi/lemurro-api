bc12435bc8dc2f16a02fd51408ae9ab405d74ff3
Дмитрий Щербаков <atomcms@ya.ru>
2018-12-24 15:41:56 +0500

    refactor: Composer каталог vendor перемещён

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Запуск приложения
  *
- * @version 13.12.2018
+ * @version 24.12.2018
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -10,7 +10,7 @@ use Lemurro\Api\Core\Core;
 
 header("Content-type: text/html; charset=UTF-8");
 
-require '../lemurro-composer-vendors/api/autoload.php';
+require '../api-vendor/autoload.php';
 
 date_default_timezone_set('UTC');
 
