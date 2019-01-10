a2de526f88e3c3291276bc788b55378b3992893f
Дмитрий Щербаков <atomcms@ya.ru>
2019-01-10 16:18:35 +0500

    refactor Входной скрипт перемещён в каталог public

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ use Lemurro\Api\Core\Core;
 
 header("Content-type: text/html; charset=UTF-8");
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 date_default_timezone_set('UTC');
 
