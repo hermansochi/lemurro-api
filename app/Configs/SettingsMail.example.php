6e923ddeaeb892aae77fa63c651d78c82e7da9e5
Дмитрий Щербаков <atomcms@ya.ru>
2019-07-16 14:06:01 +0500

    feat Добавлена возможность настроить резервный канал для отправки электронных писем

--- app/Configs/SettingsMail.example.php
+++ app/Configs/SettingsMail.example.php
@@ -2,7 +2,7 @@
 /**
  * Параметры email
  *
- * @version 01.01.2018
+ * @version 16.07.2019
  * @author  Дмитрий Щербаков <atomcms@ya.ru>
  */
 
@@ -19,6 +19,8 @@ class SettingsMail
      * Почта проекта
      */
     const APP_EMAIL = 'no-reply@domain.tld';
+
+    // ОСНОВНОЙ КАНАЛ
 
     /**
      * Отправка через SMTP с авторизацией
@@ -49,4 +51,36 @@ class SettingsMail
      * Пароль от почтового ящика
      */
     const SMTP_PASSWORD = 'PASSWORD';
+
+    // РЕЗЕРВНЫЙ КАНАЛ (ВСЕГДА SMTP)
+
+    /**
+     * Отправка через SMTP с авторизацией
+     */
+    const RESERVE = false;
+
+    /**
+     * Тип протокола (ssl|tls)
+     */
+    const RESERVE_SMTP_SECURITY = 'ssl';
+
+    /**
+     * Сервер
+     */
+    const RESERVE_SMTP_HOST = 'HOST';
+
+    /**
+     * Порт
+     */
+    const RESERVE_SMTP_PORT = 'PORT';
+
+    /**
+     * Адрес почты
+     */
+    const RESERVE_SMTP_USERNAME = 'no-reply@domain.tld';
+
+    /**
+     * Пароль от почтового ящика
+     */
+    const RESERVE_SMTP_PASSWORD = 'PASSWORD';
 }
