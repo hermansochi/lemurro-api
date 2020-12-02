f7986eaff5b5ca6638d160cc0bee68533bc80f7c
Дмитрий Щербаков <atomcms@ya.ru>
2020-12-02 14:47:05 +0500

    feat [Tests] Настройка GitHub Actions для автоматических тестов

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Lemurro API
 
+[![Lemurro API tests](https://github.com/Lemurro/api/workflows/API%20tests/badge.svg?branch=master)](https://github.com/Lemurro/api/actions?query=workflow%3A"API+tests")
 [![PHP version](https://img.shields.io/packagist/php-v/Lemurro/api-core.svg)](https://packagist.org/packages/Lemurro/api-core)
 [![License](https://img.shields.io/github/license/Lemurro/api.svg)](https://github.com/Lemurro/api)
 
--- tests/_data/database.sql
+++ tests/_data/database.sql
@@ -11,14 +11,5 @@ DELETE FROM `info_users`;
 DELETE FROM `sessions`;
 DELETE FROM `users`;
 
-LOCK TABLES `info_users` WRITE;
-/*!40000 ALTER TABLE `info_users` DISABLE KEYS */;
 INSERT INTO `info_users` VALUES (1,1,'{\"admin\": true}',NULL,'для','cli-скриптов','Пользователь','2018-04-24 00:00:00','2019-04-30 07:10:38',NULL),(2,2,'{\"admin\": true}','test@local.local','Тест','Тестович','Тестов','2018-04-24 00:00:00','2020-08-18 09:37:47',NULL);
-/*!40000 ALTER TABLE `info_users` ENABLE KEYS */;
-UNLOCK TABLES;
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'lemurro@lemurro',0,NULL,NULL,NULL),(2,'test@local.local',0,'2018-04-24 00:00:00','2020-08-18 09:37:47',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
