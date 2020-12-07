509558328008139a4d6aa399c41463aa10425e98
Дмитрий Щербаков <atomcms@ya.ru>
2020-12-07 13:15:16 +0500

    feat [Tests] Доработка dump файла чтобы не ломать существующие данные

--- tests/_data/database.sql
+++ tests/_data/database.sql
@@ -1,15 +1,45 @@
-DELETE FROM `access_sets`;
-DELETE FROM `auth_codes`;
-DELETE FROM `auth_codes_lasts`;
-DELETE FROM `data_change_logs`;
-DELETE FROM `example`;
-DELETE FROM `files`;
-DELETE FROM `files_downloads`;
-DELETE FROM `guide_example`;
-DELETE FROM `history_registrations`;
-DELETE FROM `info_users`;
-DELETE FROM `sessions`;
-DELETE FROM `users`;
+INSERT INTO
+    users
+SET
+    auth_id = 'test@local.local',
+    created_at = '2020-12-06 00:00:00',
+    deleted_at = NULL ON DUPLICATE KEY
+UPDATE
+    auth_id = 'test@local.local',
+    created_at = '2020-12-06 00:00:00',
+    deleted_at = NULL;
 
-INSERT INTO `info_users` VALUES (1,1,'{\"admin\": true}',NULL,'для','cli-скриптов','Пользователь','2018-04-24 00:00:00','2019-04-30 07:10:38',NULL),(2,2,'{\"admin\": true}','test@local.local','Тест','Тестович','Тестов','2018-04-24 00:00:00','2020-08-18 09:37:47',NULL);
-INSERT INTO `users` VALUES (1,'lemurro@lemurro',0,NULL,NULL,NULL),(2,'test@local.local',0,'2018-04-24 00:00:00','2020-08-18 09:37:47',NULL);
+INSERT INTO
+    info_users
+SET
+    user_id = (
+        SELECT
+            id
+        FROM
+            users
+        WHERE
+            auth_id = 'test@local.local'
+    ),
+    roles = '{\"admin\": true}',
+    email = 'test@local.local',
+    first_name = 'Тест',
+    second_name = 'Тестович',
+    last_name = 'Тестов',
+    created_at = '2020-12-06 00:00:00',
+    deleted_at = NULL ON DUPLICATE KEY
+UPDATE
+    user_id = (
+        SELECT
+            id
+        FROM
+            users
+        WHERE
+            auth_id = 'test@local.local'
+    ),
+    roles = '{\"admin\": true}',
+    email = 'test@local.local',
+    first_name = 'Тест',
+    second_name = 'Тестович',
+    last_name = 'Тестов',
+    created_at = '2020-12-06 00:00:00',
+    deleted_at = NULL;
