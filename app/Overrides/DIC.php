254c27fc698ecf0f81a5563b9ab23e0c2b38cc0d
DimNS <atomcms@ya.ru>
2021-11-16 10:53:18 +0500

    refactor [Overrides] Вместо bool возвращается void

--- app/Overrides/DIC.php
+++ app/Overrides/DIC.php
@@ -1,38 +1,21 @@
 <?php
-/**
- * Добавление новых элементов в DIC
- *
- * @version 19.04.2019
- * @author  Дмитрий Щербаков <atomcms@ya.ru>
- */
 
 namespace Lemurro\Api\App\Overrides;
 
 use Pimple\Container;
 
 /**
- * Class DIC
- *
- * @package Lemurro\Api\App\Overrides
+ * Добавление новых элементов в DIC
  */
 class DIC
 {
     /**
-     * Выполним действие
-     *
-     * @param Container $dic Контейнер
-     *
-     * @return boolean
-     *
-     * @version 06.06.2018
-     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     * Добавление новых элементов в DIC
      */
-    public function run(Container $dic)
+    public function run(Container $dic): void
     {
         // Здесь вы можете добавлять свои элементы в контейнер
         // Используется контейнер Pimple (https://github.com/silexphp/Pimple)
         // Пример: $dic['my-element'] = 'My super element';
-
-        return true;
     }
 }
--- app/Overrides/Response.php
+++ app/Overrides/Response.php
@@ -1,37 +1,20 @@
 <?php
-/**
- * Изменение объекта Response перед запуском контроллера приложения
- *
- * @version 19.04.2019
- * @author  Дмитрий Щербаков <atomcms@ya.ru>
- */
 
 namespace Lemurro\Api\App\Overrides;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
- * Class Response
- *
- * @package Lemurro\Api\App\Overrides
+ * Изменение объекта Response перед запуском контроллера приложения
  */
 class Response
 {
     /**
-     * Выполним действие
-     *
-     * @param JsonResponse $response Объект ответа
-     *
-     * @return boolean
-     *
-     * @version 06.06.2018
-     * @author  Дмитрий Щербаков <atomcms@ya.ru>
+     * Изменение объекта Response перед запуском контроллера приложения
      */
-    public function run(JsonResponse $response)
+    public function run(JsonResponse $response): void
     {
         // Здесь вы можете переопределить параметры ответа
         // $response->headers->set('My-Header', 'My value');
-
-        return true;
     }
 }
